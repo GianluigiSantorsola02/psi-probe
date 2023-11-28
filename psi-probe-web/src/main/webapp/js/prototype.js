0d835f5572a4433a3cdb69015470bc5d7b68faa5
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-11-28 22:19:20 +0100

    bug fixing

--- psi-probe-web/src/main/webapp/js/prototype.js
+++ psi-probe-web/src/main/webapp/js/prototype.js
@@ -744,7 +744,7 @@ Object.extend(String.prototype, (function() {
 
   function evalJSON(sanitize) {
     var json = this.unfilterJSON(),
-        cx = /[\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff\u0000]/g;
+        cx = /[\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g;
     if (cx.test(json)) {
       json = json.replace(cx, function (a) {
         return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
@@ -4633,8 +4633,8 @@ var i,
 	ridentifier = new RegExp( "^" + identifier + "$" ),
 
 	matchExpr = {
-		"ID": new RegExp( "^#(" + characterEncoding + ")" ),
-		"CLASS": new RegExp( "^\\.(" + characterEncoding + ")" ),
+      "ID": new RegExp("^#(" + characterEncoding.replace(/[\x00-\x1F\x7F-\x9F]/g, "") + ")"),
+      "CLASS": new RegExp("^\\.(" + characterEncoding.replace(/[\x00-\x1F\x7F-\x9F]/g, "") + ")"),
 		"TAG": new RegExp( "^(" + characterEncoding.replace( "w", "w*" ) + ")" ),
 		"ATTR": new RegExp( "^" + attributes ),
 		"PSEUDO": new RegExp( "^" + pseudos ),
@@ -6089,7 +6089,7 @@ function matcherFromGroupMatchers( elementMatchers, setMatchers ) {
 			}
 
 			matchedCount += i;
-			if ( bySet && i !== matchedCount ) {
+			if ( bySet && i != matchedCount ) {
 				j = 0;
 				while ( (matcher = setMatchers[j++]) ) {
 					matcher( unmatched, setMatched, context, xml );
