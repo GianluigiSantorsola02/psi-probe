16891185e44ab918e1729d8acb308ac2ad0e416e
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-11-29 12:45:57 +0100

    bug fixing

--- psi-probe-web/src/main/webapp/js/prototype.js
+++ psi-probe-web/src/main/webapp/js/prototype.js
@@ -773,7 +773,7 @@ Object.extend(String.prototype, (function() {
 
   function evalJSON(sanitize) {
     var json = this.unfilterJSON(),
-        cx = new RegExp("[\u00ad\u0600-\u0604\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]|[\u070f][\u17b4]", "g");
+        cx = new RegExp("[\u00ad\u0600-\u0604\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]|[\u070f][\u17b4]", "g");
     if (cx.test(json)) {
       json = json.replace(cx, function (a) {
         return '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
