97dd42344ec68ecd377202da48cf7be43543b426
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-11-29 11:50:58 +0100

    bug fixing

--- psi-probe-core/src/main/java/psiprobe/controllers/DecoratorController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/DecoratorController.java
@@ -21,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -76,14 +77,22 @@ public class DecoratorController extends PostParameterizableViewController {
 
 
     if (getApplicationContext() != null) {
-      DataHandler properties = (DataHandler) getApplicationContext().getBean("version");
-      Properties data = (Properties) getApplicationContext().getBean("version");
-      if (data.getProperty("probe.version") != null) {
-        request.setAttribute("version", data.getProperty("probe.version"));
-      } else {
-        // Handle the case when 'data' is null
-        logger.error("Error: 'version' bean is null");
-      }
+        DataHandler properties = (DataHandler) getApplicationContext().getBean("version");
+        ApplicationContext context = getApplicationContext();
+        Properties data = null;
+        if (context != null) {
+            data = (Properties) context.getBean("version");
+
+        } else {
+          System.out.println("ApplicationContext is null. Cannot retrieve the 'version' bean");
+        }
+        assert data != null;
+        if (data.getProperty("probe.version") != null) {
+            request.setAttribute("version", data.getProperty("probe.version"));
+        } else {
+            // Handle the case when 'data' is null
+            logger.error("Error: 'version' bean is null");
+        }
     } else {
       logger.error("ApplicationContext is null. Cannot retrieve the 'version' bean");
     }
