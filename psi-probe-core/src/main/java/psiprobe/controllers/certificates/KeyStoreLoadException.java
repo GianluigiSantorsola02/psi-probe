63ce4d2e1dc8d45840221e2f68c60287c73b37ec
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-01 15:41:48 +0100

    code smell ListCertificatesController

---
+++ psi-probe-core/src/main/java/psiprobe/controllers/certificates/KeyStoreLoadException.java
@@ -0,0 +1,7 @@
+package psiprobe.controllers.certificates;
+
+public class KeyStoreLoadException extends Exception {
+    public KeyStoreLoadException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
