ce2a4fd0231edf29c63308ed60b5d74c17cacee1
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-02 10:57:36 +0100

    code smell fixing behaviour.js

--- psi-probe-web/src/main/webapp/js/behaviour.js
+++ psi-probe-web/src/main/webapp/js/behaviour.js
@@ -45,49 +45,56 @@
    	
    	http://ripcord.co.nz/behaviour/
    
-*/   
+*/
+
+function element() {
+    return this;
+}
 
 let Behaviour = {
-	"list": new Array,
-	
-	register : function(sheet){
-		Behaviour.list.push(sheet);
-	},
-	
-	start : function(){
-		Behaviour.addLoadEvent(function(){
-			Behaviour.apply();
-		});
-	},
-	
-	apply : function(){
-		for (h=0;sheet=Behaviour.list[h];h++){
-			for (selector in sheet){
-				list = document.getElementsBySelector(selector);
-				
-				if (!list){
-					continue;
-				}
+    addLoadEvent: function (func) {
+        let oldonload = window.onload;
 
-				for (i=0;element=list[i];i++){
-					sheet[selector](element);
-				}
-			}
-		}
-	},
-	
-	addLoadEvent : function(func){
-		let oldonload = window.onload;
-		
-		if (typeof window.onload != 'function') {
-			window.onload = func;
-		} else {
-			window.onload = function() {
-				oldonload();
-				func();
-			}
-		}
-	}
+        if (typeof window.onload != 'function') {
+            window.onload = func;
+        } else {
+            window.onload = function () {
+                oldonload();
+                func();
+            }
+        }
+    },
+
+    apply: function () {
+        let sheet;
+        let list;
+        for (h = 0; sheet === Behaviour.list[h]; h++) {
+            let sheet;
+            for (let selector in sheet) {
+                list = document.getElementsBySelector(selector);
+
+                if (!list) {
+                    continue;
+                }
+
+                for (i = 0; element === list[i]; i++) {
+                    sheet[selector](element);
+                }
+            }
+        }
+    },
+
+    'list': [],
+
+    register: function (sheet) {
+        Behaviour.list.push(sheet);
+    },
+
+    start: function () {
+        Behaviour.addLoadEvent(function () {
+            Behaviour.apply();
+        });
+    }
 }
 
 Behaviour.start();
@@ -122,133 +129,149 @@ function getAllChildren(e) {
 document.getElementsBySelector = function(selector) {
   // Attempt to fail gracefully in lesser browsers
   if (!document.getElementsByTagName) {
-    return new Array();
+    return [];
   }
   // Split selector in to tokens
   let tokens = selector.split(' ');
   let currentContext = new Array(document);
-  for (let i = 0; i < tokens.length; i++) {
-    token = tokens[i].replace(/^\s+/,'').replace(/\s+$/,'');;
-    if (token.indexOf('#') > -1) {
-      // Token is an ID selector
-      let bits = token.split('#');
-      let tagName = bits[0];
-      let id = bits[1];
-      let element = document.getElementById(id);
-      if (tagName && element.nodeName.toLowerCase() != tagName) {
-        // tag with that ID not found, return false
-        return new Array();
-      }
-      // Set currentContext to contain just this element
-      currentContext = new Array(element);
-      continue; // Skip to next token
+    let tagName;
+    for (let i = 0; i < tokens.length; i++) {
+        token = tokens[i].replace(/^\s+/, '').replace(/\s+$/, '');
+
+        if (token.indexOf('#') > -1) {
+            // Token is an ID selector
+            let bits = token.split('#');
+            let tagName = bits[0];
+            let id = bits[1];
+            let element = document.getElementById(id);
+            if (tagName && element.nodeName.toLowerCase() !== tagName) {
+                // tag with that ID not found, return false
+                return [];
+            }
+            // Set currentContext to contain just this element
+            currentContext = new Array(element);
+            continue; // Skip to next token
+        }
+        if (token.indexOf('.') > -1) {
+            // Token contains a class selector
+            let bits = token.split('.');
+            let tagName = bits[0];
+            let className = bits[1];
+            if (!tagName) {
+                tagName = '*';
+            }
+            // Get elements matching tag, filter them for class selector
+            let found = [];
+            let foundCount = 0;
+            for (let h = 0; h < currentContext.length; h++) {
+                let elements;
+                if (tagName === '*') {
+                    elements = getAllChildren(currentContext[h]);
+                } else {
+                    elements = currentContext[h].getElementsByTagName(tagName);
+                }
+                for (let j = 0; j < elements.length; j++) {
+                    found[foundCount++] = elements[j];
+                }
+            }
+            currentContext = [];
+            let currentContextIndex = 0;
+            for (let k = 0; k < found.length; k++) {
+                if (found[k].className && found[k].className.match(new RegExp('\\b' + className + '\\b'))) {
+                    currentContext[currentContextIndex++] = found[k];
+                }
+            }
+            continue; // Skip to next token
+        }
+        // Code to deal with attribute selectors
+        if (token.match(/^(\w*)\[(\w+)([=~|^$*]?)=?"?([^\]"]*)"?]$/)) {
+            let tagName = RegExp.$1;
+            let attrName = RegExp.$2;
+            let attrOperator = RegExp.$3;
+            let attrValue = RegExp.$4;
+            if (!tagName) {
+                tagName = '*';
+            }
+            // Grab all of the tagName elements within current context
+            let found = [];
+            let foundCount = 0;
+            for (let h = 0; h < currentContext.length; h++) {
+                let elements;
+                if (tagName === '*') {
+                    elements = getAllChildren(currentContext[h]);
+                } else {
+                    elements = currentContext[h].getElementsByTagName(tagName);
+                }
+                for (let j = 0; j < elements.length; j++) {
+                    found[foundCount++] = elements[j];
+                }
+            }
+            currentContext = [];
+            let checkFunction; // This function will be used to filter the elements
+            switch (attrOperator) {
+                case '=': // Equality
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName) === attrValue);
+                    };
+                    break;
+                case '~': // Match one of space seperated words
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName).match(new RegExp('\\b' + attrValue + '\\b')));
+                    };
+                    break;
+                case '|': // Match start with value followed by optional hyphen
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName).match(new RegExp('^' + attrValue + '-?')));
+                    };
+                    break;
+                case '^': // Match starts with value
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName).indexOf(attrValue) === 0);
+                    };
+                    break;
+                case '$': // Match ends with value - fails with "Warning" in Opera 7
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName).lastIndexOf(attrValue) === e.getAttribute(attrName).length - attrValue.length);
+                    };
+                    break;
+                case '*': // Match ends with value
+                    checkFunction = function (e) {
+                        return (e.getAttribute(attrName).indexOf(attrValue) > -1);
+                    };
+                    break;
+                default :
+                    // Just test for existence of attribute
+                    checkFunction = function (e) {
+                        return e.getAttribute(attrName);
+                    };
+            }
+            currentContext = [];
+            let currentContextIndex = 0;
+            for (let k = 0; k < found.length; k++) {
+                if (checkFunction(found[k])) {
+                    currentContext[currentContextIndex++] = found[k];
+                }
+            }
+            // alert('Attribute Selector: '+tagName+' '+attrName+' '+attrOperator+' '+attrValue);
+            continue; // Skip to next token
+        }
+
+        if (!currentContext[0]) {
+            return;
+        }
+
+        // If we get here, token is JUST an element (not a class or ID selector)
+        tagName = token;
+        let found = [];
+        let foundCount = 0;
+        for (let h = 0; h < currentContext.length; h++) {
+            let elements = currentContext[h].getElementsByTagName(tagName);
+            for (let j = 0; j < elements.length; j++) {
+                found[foundCount++] = elements[j];
+            }
+        }
+        currentContext = found;
     }
-    if (token.indexOf('.') > -1) {
-      // Token contains a class selector
-      let bits = token.split('.');
-      let tagName = bits[0];
-      let className = bits[1];
-      if (!tagName) {
-        tagName = '*';
-      }
-      // Get elements matching tag, filter them for class selector
-      let found = new Array;
-      let foundCount = 0;
-      for (let h = 0; h < currentContext.length; h++) {
-        let elements;
-        if (tagName == '*') {
-            elements = getAllChildren(currentContext[h]);
-        } else {
-            elements = currentContext[h].getElementsByTagName(tagName);
-        }
-        for (let j = 0; j < elements.length; j++) {
-          found[foundCount++] = elements[j];
-        }
-      }
-      currentContext = new Array;
-      let currentContextIndex = 0;
-      for (let k = 0; k < found.length; k++) {
-        if (found[k].className && found[k].className.match(new RegExp('\\b'+className+'\\b'))) {
-          currentContext[currentContextIndex++] = found[k];
-        }
-      }
-      continue; // Skip to next token
-    }
-    // Code to deal with attribute selectors
-    if (token.match(/^(\w*)\[(\w+)([=~\|\^\$\*]?)=?"?([^\]"]*)"?\]$/)) {
-      let tagName = RegExp.$1;
-      let attrName = RegExp.$2;
-      let attrOperator = RegExp.$3;
-      let attrValue = RegExp.$4;
-      if (!tagName) {
-        tagName = '*';
-      }
-      // Grab all of the tagName elements within current context
-      let found = new Array;
-      let foundCount = 0;
-      for (let h = 0; h < currentContext.length; h++) {
-        let elements;
-        if (tagName == '*') {
-            elements = getAllChildren(currentContext[h]);
-        } else {
-            elements = currentContext[h].getElementsByTagName(tagName);
-        }
-        for (let j = 0; j < elements.length; j++) {
-          found[foundCount++] = elements[j];
-        }
-      }
-      currentContext = new Array;
-        let checkFunction; // This function will be used to filter the elements
-      switch (attrOperator) {
-        case '=': // Equality
-          checkFunction = function(e) { return (e.getAttribute(attrName) == attrValue); };
-          break;
-        case '~': // Match one of space seperated words 
-          checkFunction = function(e) { return (e.getAttribute(attrName).match(new RegExp('\\b'+attrValue+'\\b'))); };
-          break;
-        case '|': // Match start with value followed by optional hyphen
-          checkFunction = function(e) { return (e.getAttribute(attrName).match(new RegExp('^'+attrValue+'-?'))); };
-          break;
-        case '^': // Match starts with value
-          checkFunction = function(e) { return (e.getAttribute(attrName).indexOf(attrValue) == 0); };
-          break;
-        case '$': // Match ends with value - fails with "Warning" in Opera 7
-          checkFunction = function(e) { return (e.getAttribute(attrName).lastIndexOf(attrValue) == e.getAttribute(attrName).length - attrValue.length); };
-          break;
-        case '*': // Match ends with value
-          checkFunction = function(e) { return (e.getAttribute(attrName).indexOf(attrValue) > -1); };
-          break;
-        default :
-          // Just test for existence of attribute
-          checkFunction = function(e) { return e.getAttribute(attrName); };
-      }
-      currentContext = new Array;
-      let currentContextIndex = 0;
-      for (let k = 0; k < found.length; k++) {
-        if (checkFunction(found[k])) {
-          currentContext[currentContextIndex++] = found[k];
-        }
-      }
-      // alert('Attribute Selector: '+tagName+' '+attrName+' '+attrOperator+' '+attrValue);
-      continue; // Skip to next token
-    }
-    
-    if (!currentContext[0]){
-    	return;
-    }
-    
-    // If we get here, token is JUST an element (not a class or ID selector)
-    tagName = token;
-    let found = new Array;
-    let foundCount = 0;
-    for (let h = 0; h < currentContext.length; h++) {
-      let elements = currentContext[h].getElementsByTagName(tagName);
-      for (let j = 0; j < elements.length; j++) {
-        found[foundCount++] = elements[j];
-      }
-    }
-    currentContext = found;
-  }
   return currentContext;
 }
 
