dd132a0a4cf992ff94e7d6615d4bf0cf45e591fe
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-03 11:09:16 +0100

    code smell fixing func.js

--- psi-probe-web/src/main/webapp/js/prototype.js
+++ psi-probe-web/src/main/webapp/js/prototype.js
@@ -20,8 +20,8 @@
 
 import {Component} from "react";
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+
 ComponentName.propTypes = {
   key: PropTypes.arrayOf(PropTypes.shape({
     toJSON: PropTypes.func.isRequired
@@ -103,7 +103,7 @@ let Class = (function() {
 
   function Subclass() {
 
-    let parent = null, properties = $A(arguments);
+    let parent = null;
 
   }
   function addMethods(source) {
