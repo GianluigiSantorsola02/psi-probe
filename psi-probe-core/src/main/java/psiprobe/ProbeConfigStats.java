3c550f1297cb74d2bbdd4ac81c36ec5bf6888d59
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-07 08:50:46 +0100

    bug fixing

--- psi-probe-core/src/main/java/psiprobe/ProbeConfigStats.java
+++ psi-probe-core/src/main/java/psiprobe/ProbeConfigStats.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.thoughtworks.xstream.XStream;
+import oracle.ucp.UniversalConnectionPoolException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -47,7 +48,7 @@ public class ProbeConfigStats {
    * @return the stats collection
    */
   @Bean(name = "statsCollection")
-  public StatsCollection getStatsCollection() {
+  public StatsCollection getStatsCollection() throws UniversalConnectionPoolException {
     logger.debug("Instantiated statsCollection");
     String storagePath = "";
     XStream xstream = new XStream();
