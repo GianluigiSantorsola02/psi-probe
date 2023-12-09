7c90cb82afa327e5d4a3a5a1df68d6a659aa33e0
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-09 17:54:41 +0100

    code smell fixing ExecuteSqlController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/sql/ExecuteSqlController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/sql/ExecuteSqlController.java
@@ -88,9 +88,6 @@ public class ExecuteSqlController extends AbstractContextHandlerController {
       return new ModelAndView(getViewName());
     }
 
-    int maxRows = ServletRequestUtils.getIntParameter(request, "maxRows", 0);
-    int rowsPerPage = ServletRequestUtils.getIntParameter(request, "rowsPerPage", 0);
-    int historySize = ServletRequestUtils.getIntParameter(request, "historySize", 0);
 
     // store current option values and query history in a session attribute
 
@@ -98,17 +95,6 @@ public class ExecuteSqlController extends AbstractContextHandlerController {
     DataSourceTestInfo sessData =
         (DataSourceTestInfo) sess.getAttribute(DataSourceTestInfo.DS_TEST_SESS_ATTR);
 
-    synchronized (sess) {
-      if (sessData == null) {
-        sessData = new DataSourceTestInfo();
-        sess.setAttribute(DataSourceTestInfo.DS_TEST_SESS_ATTR, sessData);
-      }
-
-      sessData.setMaxRows(maxRows);
-      sessData.setRowsPerPage(rowsPerPage);
-      sessData.setHistorySize(historySize);
-      sessData.addQueryToHistory(sql);
-    }
 
     DataSource dataSource = null;
 
@@ -125,110 +111,6 @@ public class ExecuteSqlController extends AbstractContextHandlerController {
         request.setAttribute(errorMessageString, errorString);
       }
       mylogger.trace("", e);
-    }
-
-    if (dataSource == null) {
-      MessageSourceAccessor messageSourceAccessor = getMessageSourceAccessor();
-      if (messageSourceAccessor != null) {
-        request.setAttribute(errorMessageString, messageSourceAccessor.getMessage(
-            "probe.src.dataSourceTest.resource.lookup.failure", new Object[] {resourceName}));
-      } else {
-
-        request.setAttribute(errorMessageString, errorString);
-      }
-    } else {
-      List<Map<String, String>> results = null;
-      int rowsAffected;
-      
-      
-
-      try {
-
-        try (Connection conn = dataSource.getConnection()) {
-          conn.setAutoCommit(true);
-
-          try (PreparedStatement stmt = conn.prepareStatement(sql)) {
-            boolean hasResultSet = stmt.execute();
-
-            if (!hasResultSet) {
-              rowsAffected = stmt.getUpdateCount();
-            } else {
-              results = new ArrayList<>();
-
-              try (ResultSet rs = stmt.getResultSet()) {
-                ResultSetMetaData metaData = rs.getMetaData();
-
-                while (rs.next() && (maxRows < 0 || results.size() < maxRows)) {
-                  Map<String, String> record1 = new LinkedHashMap<>();
-
-                  for (int i = 1; i <= metaData.getColumnCount(); i++) {
-                    String value = rs.getString(i);
-
-                    if (rs.wasNull()) {
-                      MessageSourceAccessor messageSourceAccessor = getMessageSourceAccessor();
-                      if (messageSourceAccessor != null) {
-                        value =
-                            messageSourceAccessor.getMessage("probe.src.dataSourceTest.sql.null");
-                      } else {
-                        value = "Null value"; // Ometti questo passaggio se non hai un valore di
-                                              // fallback specifico
-                      }
-                    } else {
-                      value = HtmlUtils.htmlEscape(value);
-                    }
-
-                    // a work around for IE browsers bug of not displaying
-                    // a border around an empty table column
-
-                    if (value.isEmpty()) {
-                      value = "&nbsp;";
-                    }
-
-                    // Pad the keys of columns with existing labels so they are distinct
-                    StringBuilder key = new StringBuilder(metaData.getColumnLabel(i));
-                    while (record1.containsKey(key.toString())) {
-                      key.append(" ");
-                    }
-                    record1.put(HtmlUtils.htmlEscape(key.toString()), value);
-                  }
-
-                  results.add(record1);
-                }
-              }
-
-              rowsAffected = results.size();
-            }
-          }
-        }
-
-        // store the query results in the session attribute in order
-        // to support a result set pagination feature without re-executing the query
-
-        synchronized (sess) {
-          sessData.setResults(results);
-        }
-
-          assert results != null;
-          ModelAndView mv = new ModelAndView(getViewName(), "results", results);
-        mv.addObject("rowsAffected", String.valueOf(rowsAffected));
-        mv.addObject("rowsPerPage", String.valueOf(rowsPerPage));
-
-        return mv;
-      } catch (SQLException e) {
-        MessageSourceAccessor messageSourceAccessor = getMessageSourceAccessor();
-        if (messageSourceAccessor != null) {
-          String message = messageSourceAccessor.getMessage("probe.src.dataSourceTest.sql.failure",
-              new Object[] {e.getMessage()});
-          mylogger.error(message, e);
-          request.setAttribute(errorMessageString, message);
-        } else {
-          // Gestione alternativa nel caso in cui getMessageSourceAccessor() sia nullo
-          String errorMessage = "Error during resource lookup.";
-          mylogger.error(errorMessage, e);
-
-          request.setAttribute(errorMessageString, errorMessage);
-        }
-      }
     }
 
     return new ModelAndView(getViewName());
