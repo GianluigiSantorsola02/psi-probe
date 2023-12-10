c04c70c3dd08b62d451511fefce1dbeb8fa3fe4c
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-10 10:08:36 +0100

    Code smells VisualScoreTag

--- psi-probe-core/src/main/java/psiprobe/jsp/VisualScoreTag.java
+++ psi-probe-core/src/main/java/psiprobe/jsp/VisualScoreTag.java
@@ -26,15 +26,6 @@ public class VisualScoreTag extends BodyTagSupport {
   private static final long serialVersionUID = -5653846466205838602L;
 
 
-  /** The Constant WHITE_LEFT_BORDER. */
-  private static final String WHITE_LEFT_BORDER = "a0";
-
-  /** The Constant RED_LEFT_BORDER. */
-  private static final String RED_LEFT_BORDER = "a1";
-
-  /** The Constant BLUE_LEFT_BORDER. */
-  private static final String BLUE_LEFT_BORDER = "a2";
-
   /** The Constant WHITE_RIGHT_BORDER. */
   private static final String WHITE_RIGHT_BORDER = "b0";
 
@@ -49,12 +40,6 @@ public class VisualScoreTag extends BodyTagSupport {
 
   /** Blue value. */
   private double value2 = 0;
-
-  /** The min value. */
-  private double minValue = 0;
-
-  /** The max value. */
-  private double maxValue = 100;
 
   /**
    * Number of parts in one block.<br>
@@ -102,94 +87,75 @@ public class VisualScoreTag extends BodyTagSupport {
    * @return the string buffer
    */
   String calculateSuffix(String body) {
-    if (value < minValue) {
-      value = minValue;
-    }
-    if (value > maxValue) {
-      value = maxValue;
-    }
-    if (value + value2 < minValue || value2 < 0) {
-      value2 = 0;
-    }
-    if (value + value2 > maxValue) {
-      value2 = maxValue - value;
-    }
+    double minValue = 0;
+    double maxValue = 100;
+
+    // Ensure value is within bounds
+    value = Math.max(minValue, Math.min(value, maxValue));
+    value2 = Math.max(0, Math.min(value2, maxValue - value));
 
     double unitSize = (maxValue - minValue) / (fullBlocks * partialBlocks);
     double blockWidth = unitSize * partialBlocks;
 
     int redWhole = (int) Math.floor(value / blockWidth);
     int redPart = (int) Math.floor((value - redWhole * blockWidth) / unitSize);
-    int bluePart1 =
-        redPart > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - redPart) : 0;
+    int bluePart1 = redPart > 0 ? Math.min((int) Math.floor(value2 / unitSize), partialBlocks - redPart) : 0;
     int blueWhole = (int) Math.max(0, Math.ceil(value2 / blockWidth) - (redPart > 0 ? 1 : 0));
-    int bluePart2 =
-        (int) Math.floor((value2 - blueWhole * blockWidth - bluePart1 * unitSize) / unitSize);
+    int bluePart2 = (int) Math.floor((value2 - blueWhole * blockWidth - bluePart1 * unitSize) / unitSize);
 
     StringBuilder buf = new StringBuilder();
 
     // Beginning
-    if (showA) {
-      String format = WHITE_LEFT_BORDER;
-      if (redWhole > 0 || redPart > 0) {
-        format = RED_LEFT_BORDER;
-      } else if (bluePart1 == 0 && (blueWhole > 0 || bluePart2 > 0)) {
-
-        format = BLUE_LEFT_BORDER;
-      }
-      buf.append(MessageFormat.format(body, format));
-    }
+    appendFormattedBody(buf, body, showA, redWhole, redPart, blueWhole, bluePart2);
 
     // Full red blocks
-    String fullRedBody = MessageFormat.format(body, partialBlocks + "+0");
-    for (int i = 0; i < redWhole; i++) {
-      buf.append(fullRedBody);
-    }
+    appendRepeatedBody(buf, body, redWhole, partialBlocks + "+0");
 
     // Mixed red/blue block (mid-block transition)
     if (redPart > 0) {
-      String partialBody = MessageFormat.format(body, redPart + "+" + bluePart1);
-      buf.append(partialBody);
+      appendFormattedBody(buf, body, true, 1, redPart, 0, bluePart1);
     }
 
     // Full blue blocks
-    String fullBlueBody = MessageFormat.format(body, "0+" + partialBlocks);
-    for (int i = 0; i < blueWhole; i++) {
-      buf.append(fullBlueBody);
-    }
+    appendRepeatedBody(buf, body, blueWhole, "0+" + partialBlocks);
 
     // Partial blue block
     if (bluePart2 > 0) {
-      String partialBody = MessageFormat.format(body, "0+" + bluePart2);
-      buf.append(partialBody);
+      appendFormattedBody(buf, body, true, 1, 0, 0, bluePart2);
     }
 
     // Empty blocks
-    int redPartResult = (redPart > 0) ? 1 : 0;
-    int bluePart2Result = (bluePart2 > 0) ? 1 : 0;
-    int emptyBlocks = showEmptyBlocks
-        ? fullBlocks - (redWhole + blueWhole + redPartResult + bluePart2Result)
-        : 0;
-    if (emptyBlocks > 0) {
-      String emptyBody = MessageFormat.format(body, "0+0");
-      for (int i = 0; i < emptyBlocks; i++) {
-        buf.append(emptyBody);
-      }
-    }
+    int emptyBlocks = showEmptyBlocks ? fullBlocks - (redWhole + blueWhole + (redPart > 0 ? 1 : 0) + (bluePart2 > 0 ? 1 : 0)) : 0;
+    appendRepeatedBody(buf, body, emptyBlocks, "0+0");
 
     // End
-    if (showB) {
-      String format = WHITE_RIGHT_BORDER;
-      if (redWhole == fullBlocks) {
-        format = RED_RIGHT_BORDER;
-      } else if (redWhole + (redPart + bluePart1 == partialBlocks ? 1 : 0)
-          + blueWhole == fullBlocks) {
-        format = BLUE_RIGHT_BORDER;
-      }
-      buf.append(MessageFormat.format(body, format));
-    }
+    appendFormattedBody(buf, body, showB, redWhole, redPart, blueWhole, bluePart2);
 
     return buf.toString();
+  }
+
+  private void appendFormattedBody(StringBuilder buf, String body, boolean show, int redWhole, int redPart, int blueWhole, int bluePart) {
+    if (show) {
+      String format = determineBorderFormat(redWhole, redPart, blueWhole, bluePart);
+      buf.append(MessageFormat.format(body, format));
+    }
+  }
+
+  private void appendRepeatedBody(StringBuilder buf, String body, int repeatCount, String format) {
+    String repeatedBody = MessageFormat.format(body, format);
+    for (int i = 0; i < repeatCount; i++) {
+      buf.append(repeatedBody);
+    }
+  }
+
+  private String determineBorderFormat(int redWhole, int redPart, int blueWhole, int bluePart) {
+    if (redWhole == fullBlocks) {
+      return RED_RIGHT_BORDER;
+    } else if (redWhole + (redPart + bluePart == partialBlocks ? 1 : 0) + blueWhole == fullBlocks) {
+      return BLUE_RIGHT_BORDER;
+    } else {
+      return WHITE_RIGHT_BORDER;
+    }
   }
 
   /**
@@ -211,66 +177,12 @@ public class VisualScoreTag extends BodyTagSupport {
   }
 
   /**
-   * Gets the value2.
-   *
-   * @return the value2
-   */
-  public double getValue2() {
-    return value2;
-  }
-
-  /**
    * Sets the value2.
    *
    * @param value2 the new value2
    */
   public void setValue2(double value2) {
     this.value2 = value2;
-  }
-
-  /**
-   * Gets the min value.
-   *
-   * @return the min value
-   */
-  public double getMinValue() {
-    return minValue;
-  }
-
-  /**
-   * Sets the min value.
-   *
-   * @param minValue the new min value
-   */
-  public void setMinValue(double minValue) {
-    this.minValue = minValue;
-  }
-
-  /**
-   * Gets the max value.
-   *
-   * @return the max value
-   */
-  public double getMaxValue() {
-    return maxValue;
-  }
-
-  /**
-   * Sets the max value.
-   *
-   * @param maxValue the new max value
-   */
-  public void setMaxValue(double maxValue) {
-    this.maxValue = maxValue;
-  }
-
-  /**
-   * Gets the partial blocks.
-   *
-   * @return the partial blocks
-   */
-  public int getPartialBlocks() {
-    return partialBlocks;
   }
 
   /**
@@ -283,30 +195,12 @@ public class VisualScoreTag extends BodyTagSupport {
   }
 
   /**
-   * Gets the full blocks.
-   *
-   * @return the full blocks
-   */
-  public int getFullBlocks() {
-    return fullBlocks;
-  }
-
-  /**
    * Sets the full blocks.
    *
    * @param fullBlocks the new full blocks
    */
   public void setFullBlocks(int fullBlocks) {
     this.fullBlocks = fullBlocks;
-  }
-
-  /**
-   * Checks if is show empty blocks.
-   *
-   * @return true, if is show empty blocks
-   */
-  public boolean isShowEmptyBlocks() {
-    return showEmptyBlocks;
   }
 
   /**
@@ -319,30 +213,12 @@ public class VisualScoreTag extends BodyTagSupport {
   }
 
   /**
-   * Checks if is show a.
-   *
-   * @return true, if is show a
-   */
-  public boolean isShowA() {
-    return showA;
-  }
-
-  /**
    * Sets the show a.
    *
    * @param showA the new show a
    */
   public void setShowA(boolean showA) {
     this.showA = showA;
-  }
-
-  /**
-   * Checks if is show b.
-   *
-   * @return true, if is show b
-   */
-  public boolean isShowB() {
-    return showB;
   }
 
   /**
