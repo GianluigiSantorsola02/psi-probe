ebf8377e2fa9712f4d58af529fd4666b7ed0541d
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-10 09:51:56 +0100

    Code smells BaseTomcatAvailabilityController

--- psi-probe-core/src/main/java/psiprobe/controllers/quickcheck/BaseTomcatAvailabilityController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/quickcheck/BaseTomcatAvailabilityController.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.naming.NamingException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -35,52 +36,55 @@ import psiprobe.model.TomcatTestReport;
  */
 public class BaseTomcatAvailabilityController extends AbstractTomcatContainerController {
 
-  @Override
-  public ModelAndView handleRequestInternal(HttpServletRequest request,
-      HttpServletResponse response) throws Exception {
 
+  @Override
+  public ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
     final long start = System.currentTimeMillis();
     TomcatTestReport tomcatTestReport = new TomcatTestReport();
 
-    // check datasource status
-    tomcatTestReport.setDatasourceUsageScore(0);
+    checkDatasourceStatus(tomcatTestReport);
+    performMemoryTest(tomcatTestReport);
+    performFileTest(tomcatTestReport);
 
+    tomcatTestReport.setTestDuration(System.currentTimeMillis() - start);
+    tomcatTestReport.setMaxServiceTime(0);
+
+    return new ModelAndView(getViewName(), "testReport", tomcatTestReport);
+  }
+
+  private void checkDatasourceStatus(TomcatTestReport tomcatTestReport) throws NamingException {
     boolean allContextsAvailable = true;
+
     if (getContainerWrapper().getResourceResolver().supportsPrivateResources()) {
       for (Context appContext : getContainerWrapper().getTomcatContainer().findContexts()) {
-        allContextsAvailable = allContextsAvailable
-            && getContainerWrapper().getTomcatContainer().getAvailable(appContext);
+        allContextsAvailable = allContextsAvailable && getContainerWrapper().getTomcatContainer().getAvailable(appContext);
 
         List<ApplicationResource> applicationResources = getContainerWrapper().getResourceResolver()
-            .getApplicationResources(appContext, getContainerWrapper());
+                .getApplicationResources(appContext, getContainerWrapper());
 
-        for (ApplicationResource appResource : applicationResources) {
-          DataSourceInfo dsi = appResource.getDataSourceInfo();
-          if (dsi != null && dsi.getBusyScore() > tomcatTestReport.getDatasourceUsageScore()) {
-            tomcatTestReport.setContextName(appContext.getName());
-            tomcatTestReport.setDatasourceUsageScore(dsi.getBusyScore());
-            tomcatTestReport.setDataSourceName(appResource.getName());
-          }
-        }
+        updateDatasourceInfo(tomcatTestReport, applicationResources);
       }
 
-      tomcatTestReport.setWebappAvailabilityTest(
-          allContextsAvailable ? TomcatTestReport.TEST_PASSED : TomcatTestReport.TEST_FAILED);
-
+      tomcatTestReport.setWebappAvailabilityTest(allContextsAvailable ? TomcatTestReport.TEST_PASSED : TomcatTestReport.TEST_FAILED);
     } else {
-      List<ApplicationResource> resources =
-          getContainerWrapper().getResourceResolver().getApplicationResources();
-      for (ApplicationResource resource : resources) {
-        DataSourceInfo dsi = resource.getDataSourceInfo();
-        if (dsi != null && dsi.getBusyScore() > tomcatTestReport.getDatasourceUsageScore()) {
-          tomcatTestReport.setDatasourceUsageScore(dsi.getBusyScore());
-          tomcatTestReport.setDataSourceName(resource.getName());
-        }
+      List<ApplicationResource> resources = getContainerWrapper().getResourceResolver().getApplicationResources();
+      updateDatasourceInfo(tomcatTestReport, resources);
+    }
+
+    tomcatTestReport.setDatasourceTest(TomcatTestReport.TEST_PASSED);
+  }
+
+  private void updateDatasourceInfo(TomcatTestReport tomcatTestReport, List<ApplicationResource> resources) {
+    for (ApplicationResource resource : resources) {
+      DataSourceInfo dsi = resource.getDataSourceInfo();
+      if (dsi != null && dsi.getBusyScore() > tomcatTestReport.getDatasourceUsageScore()) {
+        tomcatTestReport.setDatasourceUsageScore(dsi.getBusyScore());
+        tomcatTestReport.setDataSourceName(resource.getName());
       }
     }
-    tomcatTestReport.setDatasourceTest(TomcatTestReport.TEST_PASSED);
+  }
 
-    // try to allocate some memory
+  private void performMemoryTest(TomcatTestReport tomcatTestReport) {
     String word = "hello";
     int count = TomcatTestReport.DEFAULT_MEMORY_SIZE / word.length();
 
@@ -94,8 +98,9 @@ public class BaseTomcatAvailabilityController extends AbstractTomcatContainerCon
       tomcatTestReport.setMemoryTest(TomcatTestReport.TEST_FAILED);
       logger.trace("", e);
     }
+  }
 
-    // try to open some files
+  private void performFileTest(TomcatTestReport tomcatTestReport) {
     File tmpDir = new File(System.getProperty("java.io.tmpdir"));
     int fileCount = tomcatTestReport.getDefaultFileCount();
     List<File> files = new ArrayList<>();
@@ -115,25 +120,29 @@ public class BaseTomcatAvailabilityController extends AbstractTomcatContainerCon
       tomcatTestReport.setFileTest(TomcatTestReport.TEST_FAILED);
       logger.trace("", e);
     } finally {
-      for (OutputStream fileStream : fileStreams) {
-        try {
-          fileStream.close();
-        } catch (IOException e) {
-          logger.trace("", e);
-        }
-      }
-      for (File file : files) {
-        Files.delete(file.toPath());
+      closeFileStreams(fileStreams);
+      deleteFiles(files);
+    }
+  }
+
+  private void closeFileStreams(List<OutputStream> fileStreams) {
+    for (OutputStream fileStream : fileStreams) {
+      try {
+        fileStream.close();
+      } catch (IOException e) {
+        logger.trace("", e);
       }
     }
+  }
 
-    tomcatTestReport.setTestDuration(System.currentTimeMillis() - start);
-
-    long maxServiceTime = 0;
-
-    tomcatTestReport.setMaxServiceTime(maxServiceTime);
-
-    return new ModelAndView(getViewName(), "testReport", tomcatTestReport);
+  private void deleteFiles(List<File> files) {
+    for (File file : files) {
+      try {
+        Files.delete(file.toPath());
+      } catch (IOException e) {
+        logger.trace("", e);
+      }
+    }
   }
 
 }
