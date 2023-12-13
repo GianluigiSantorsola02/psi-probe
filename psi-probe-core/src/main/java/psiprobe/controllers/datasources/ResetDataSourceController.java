21cdc62941cb6dad9000d36add344d130a33c239
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 18:04:23 +0100

    security hotspot ResetDataSourceController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/datasources/ResetDataSourceController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/datasources/ResetDataSourceController.java
@@ -17,7 +17,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import psiprobe.controllers.AbstractContextHandlerController;
@@ -55,7 +55,7 @@ public class ResetDataSourceController extends AbstractContextHandlerController 
     this.replacePattern = replacePattern;
   }
 
-  @RequestMapping(path = "/app/resetds.htm")
+  @GetMapping(path = "/app/resetds.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
