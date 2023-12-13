b015a2e99b1333f71e41124c5f433143bb6bee31
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 17:16:37 +0100

    security hotspot ListAppInitParamsController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/apps/ListAppInitParamsController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/apps/ListAppInitParamsController.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.catalina.Context;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -29,7 +30,7 @@ import psiprobe.tools.SecurityUtils;
 @Controller
 public class ListAppInitParamsController extends AbstractContextHandlerController {
 
-  @RequestMapping(path = "/appinitparams.htm")
+  @GetMapping(path = "/appinitparams.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
