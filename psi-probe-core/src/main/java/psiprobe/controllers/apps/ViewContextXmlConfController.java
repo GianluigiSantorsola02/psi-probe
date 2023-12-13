40613713c133bd51149ab6bbf15344e2f58e787a
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 17:25:23 +0100

    security hotspot ViewContextXmlConfController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/apps/ViewContextXmlConfController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/apps/ViewContextXmlConfController.java
@@ -10,13 +10,13 @@
  */
 package psiprobe.controllers.apps;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * The Class ViewContextXmlConfController.
@@ -24,7 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ViewContextXmlConfController extends BaseViewXmlConfController {
 
-  @RequestMapping(path = "/adm/viewcontextxml.htm")
+  @GetMapping(path = "/adm/viewcontextxml.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
