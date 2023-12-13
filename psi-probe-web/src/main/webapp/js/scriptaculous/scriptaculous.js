f5ab48af4a212a47258fa5c6bdac51fb7430745b
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-13 21:21:24 +0100

    security hotspot scriptaculous.js

--- psi-probe-web/src/main/webapp/js/scriptaculous/scriptaculous.js
+++ psi-probe-web/src/main/webapp/js/scriptaculous/scriptaculous.js
@@ -34,7 +34,7 @@ let Scriptaculous = {
       return s.src.match(js);
     }).each(function(s) {
       let path = s.src.replace(js, ''),
-      includes = s.src.match(/\?.*load=([a-z,]*)/);
+          includes = s.src.match(/\?[^&]*load=([a-z,]*)/);
       (includes ? includes[1] : 'builder,effects,dragdrop,controls,slider,sound').split(',').each(
        function(include) { Scriptaculous.require(path+include+'.js') });
     });
