22b0ea0cdb9e50a67b9a2150d9740b0d3ab00586
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 18:07:45 +0100

    security hotspot DeployController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/deploy/DeployController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/deploy/DeployController.java
@@ -11,23 +11,20 @@
 package psiprobe.controllers.deploy;
 
 import com.google.common.base.Strings;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.apache.catalina.Context;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+import psiprobe.controllers.AbstractTomcatContainerController;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import org.apache.catalina.Context;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import psiprobe.controllers.AbstractTomcatContainerController;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Precharges the list of contexts in the deploy page.
@@ -35,7 +32,7 @@ import psiprobe.controllers.AbstractTomcatContainerController;
 @Controller
 public class DeployController extends AbstractTomcatContainerController {
 
-  @RequestMapping(path = "/adm/deploy.htm")
+  @GetMapping(path = "/adm/deploy.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
