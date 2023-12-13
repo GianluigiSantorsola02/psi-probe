b16b1f1c4461503fef12d9be919512ab62dc43a5
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 12:20:27 +0100

    security hotspot fix

--- psi-probe-core/src/main/java/psiprobe/controllers/DecoratorController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/DecoratorController.java
@@ -26,6 +26,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -51,7 +52,7 @@ public class DecoratorController extends PostParameterizableViewController {
     this.messagesBasename = messagesBasename;
   }
 
-  @RequestMapping(path = "/decorator.htm")
+  @GetMapping(path = "/decorator.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
