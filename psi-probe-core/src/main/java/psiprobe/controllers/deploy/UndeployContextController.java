be7af5c3ba68e548655a6ef9f7e964253056400b
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 18:08:44 +0100

    security hotspot UndeployContextController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/deploy/UndeployContextController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/deploy/UndeployContextController.java
@@ -10,13 +10,13 @@
  */
 package psiprobe.controllers.deploy;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * The Class UndeployContextController.
@@ -24,7 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class UndeployContextController extends BaseUndeployContextController {
 
-  @RequestMapping(path = "/adm/undeploy.htm")
+  @GetMapping(path = "/adm/undeploy.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
