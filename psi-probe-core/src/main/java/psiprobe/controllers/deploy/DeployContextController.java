3c3a251b94ce1288ce8dfd921826b7a5d5d0b89a
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2023-12-13 18:06:55 +0100

    security hotspot DeployContextController.java

--- psi-probe-core/src/main/java/psiprobe/controllers/deploy/DeployContextController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/deploy/DeployContextController.java
@@ -10,20 +10,19 @@
  */
 package psiprobe.controllers.deploy;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.ServletRequestUtils;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.InternalResourceView;
-
 import psiprobe.controllers.AbstractTomcatContainerController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Forces Tomcat to install a pre-configured context name.
@@ -31,7 +30,7 @@ import psiprobe.controllers.AbstractTomcatContainerController;
 @Controller
 public class DeployContextController extends AbstractTomcatContainerController {
 
-  @RequestMapping(path = "/adm/deploycontext.htm")
+  @GetMapping(path = "/adm/deploycontext.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
       throws Exception {
