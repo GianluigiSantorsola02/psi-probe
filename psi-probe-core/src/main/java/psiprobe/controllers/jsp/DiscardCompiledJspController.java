6f130cb6ceee1167f75c23fd96c5e67a8e6ef9f2
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-14 17:56:07 +0100

    DiscardCompiledJspController.java vulnerability

--- psi-probe-core/src/main/java/psiprobe/controllers/jsp/DiscardCompiledJspController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/jsp/DiscardCompiledJspController.java
@@ -39,24 +39,8 @@ public class DiscardCompiledJspController extends AbstractContextHandlerControll
                                     HttpServletRequest request, HttpServletResponse response) {
 
     getContainerWrapper().getTomcatContainer().discardWorkDir(context);
-    // Assuming getViewName() returns user-controlled input
-    String viewName = sanitizeInput(getViewName());
-
-// Validate and sanitize the query string
-    String sanitizedQueryString = sanitizeInput(request.getQueryString());
-
-    String redirectURL = String.format("%s%s?%s",
-            request.getContextPath(),
-            viewName,
-            sanitizedQueryString);
-
-// Perform the redirect
-    return new ModelAndView(new RedirectView(redirectURL));
-
-  }
-
-  private String sanitizeInput(String viewName) {
-    return viewName;
+    return new ModelAndView(new RedirectView(
+        request.getContextPath() + getViewName() + "?" + request.getQueryString()));
   }
 
   @Value("/app/jsp.htm")
