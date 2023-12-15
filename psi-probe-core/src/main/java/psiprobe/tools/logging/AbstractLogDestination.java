bf8fdece0cb04e58f386b4aafb76d164f24a03e8
vincenzomorelli0 <v.morelli9@studenti.uniba.it>
2023-12-15 16:54:24 +0100

    fortify security issue AbstractLogDestination.java

--- psi-probe-core/src/main/java/psiprobe/tools/logging/AbstractLogDestination.java
+++ psi-probe-core/src/main/java/psiprobe/tools/logging/AbstractLogDestination.java
@@ -9,72 +9,89 @@
  * PURPOSE.
  */
 package psiprobe.tools.logging;
-
 import java.io.File;
 import java.sql.Timestamp;
-
+import java.io.IOException;
+import java.net.URI;
 /**
  * The Class AbstractLogDestination.
  */
 public abstract class AbstractLogDestination extends DefaultAccessor implements LogDestination {
-
   @Override
   public boolean isRoot() {
     return false;
   }
-
   @Override
   public boolean isContext() {
     return false;
   }
-
   @Override
   public String getIndex() {
     return null;
   }
-
   @Override
   public String getConversionPattern() {
     return null;
   }
-
   /**
    * Gets the stdout file.
    *
    * @return the stdout file
    */
   protected File getStdoutFile() {
+    ensurePathIsRelative(System.getProperty("catalina.base"));
+    ensurePathIsRelative(new File("stdout").toURI());
     File file = new File(System.getProperty("catalina.base"), "logs/catalina.out");
     return file.exists() ? file : new File("stdout");
   }
+  private static void ensurePathIsRelative(String path) {
+    ensurePathIsRelative(new File(path));
+  }
+  private static void ensurePathIsRelative(URI uri) {
+    ensurePathIsRelative(new File(uri));
+  }
+  private static void ensurePathIsRelative(File file) {
 
+    String canonicalPath;
+    String absolutePath;
+
+    if (file.isAbsolute()) {
+      throw new RuntimeException("Potential directory traversal attempt - absolute path not allowed");
+    }
+
+    try {
+      canonicalPath = file.getCanonicalPath();
+      absolutePath = file.getAbsolutePath();
+    } catch (IOException e) {
+      throw new RuntimeException("Potential directory traversal attempt", e);
+    }
+
+    if (!canonicalPath.startsWith(absolutePath) || !canonicalPath.equals(absolutePath)) {
+      throw new RuntimeException("Potential directory traversal attempt");
+    }
+  }
   @Override
   public File getFile() {
     return getStdoutFile();
   }
-
   @Override
   public long getSize() {
     File file = getFile();
     return file != null && file.exists() ? file.length() : 0;
   }
-
   @Override
   public Timestamp getLastModified() {
     File file = getFile();
     return file != null && file.exists() ? new Timestamp(file.lastModified()) : null;
   }
-
   @Override
   public String getLevel() {
     return null;
   }
-
   @Override
   public String[] getValidLevels() {
     return new String[0];
   }
-
   @Override
   public String getEncoding() {
     return null;
