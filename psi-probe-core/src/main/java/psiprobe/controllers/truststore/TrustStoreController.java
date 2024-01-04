cceb3d56798e74a812b8807579bdee916750400e
GianluigiSantorsola <g.santorsola5@studenti.uniba.it>
2024-01-04 17:30:42 +0100

    Revert "code smell fixing TrustStoreController.java"
    This reverts commit 17abd474

--- psi-probe-core/src/main/java/psiprobe/controllers/truststore/TrustStoreController.java
+++ psi-probe-core/src/main/java/psiprobe/controllers/truststore/TrustStoreController.java
@@ -5,43 +5,57 @@
  *   https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  *
  * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
- * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTIBILITY AND FITNESS FOR A PARTICULAR
  * PURPOSE.
  */
 package psiprobe.controllers.truststore;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import psiprobe.beans.ContainerListenerBean;
 import psiprobe.controllers.AbstractTomcatContainerController;
+import psiprobe.controllers.certificates.KeyStoreLoadException;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+
+/**
+ * The Class TrustStoreController.
+ */
 @Controller
 public class TrustStoreController extends AbstractTomcatContainerController {
 
+
+  /** The Constant logger. */
   private static final Logger mylogger = LoggerFactory.getLogger(TrustStoreController.class);
 
-  @GetMapping(path = "/truststore.htm")
+  @RequestMapping(path = "/truststore.htm")
   @Override
   public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
-          throws Exception {
+      throws Exception {
     return super.handleRequest(request, response);
   }
 
   @Override
   protected ModelAndView handleRequestInternal(HttpServletRequest request,
-                                               HttpServletResponse response) throws Exception {
+      HttpServletResponse response) throws Exception {
     List<Map<String, String>> certificateList = new ArrayList<>();
     try {
       String trustStoreType = System.getProperty("javax.net.ssl.trustStoreType");
@@ -51,10 +65,11 @@ public class TrustStoreController extends AbstractTomcatContainerController {
       } else {
         ks = KeyStore.getInstance("JKS");
       }
+      String trustStore = System.getProperty("javax.net.ssl.trustStore");
+      String trustStorePassword = System.getProperty("javax.net.ssl.trustStorePassword");
+      if (trustStore != null) {
+        loadKeyStore(Files.newInputStream(Paths.get(trustStore)), trustStorePassword.toCharArray());
 
-      // Validate and sanitize user input (trustStore)
-      String trustStorePath = System.getProperty("javax.net.ssl.trustStore");
-      if (StringUtils.isNotBlank(trustStorePath)) {
         Map<String, String> attributes;
         for (String alias : Collections.list(ks.aliases())) {
           attributes = new HashMap<>();
@@ -64,7 +79,7 @@ public class TrustStoreController extends AbstractTomcatContainerController {
             attributes.put("alias", alias);
             attributes.put("cn", cert.getSubjectX500Principal().toString());
             attributes.put("expirationDate",
-                    new SimpleDateFormat("yyyy-MM-dd").format(cert.getNotAfter()));
+                new SimpleDateFormat("yyyy-MM-dd").format(cert.getNotAfter()));
             certificateList.add(attributes);
           }
         }
@@ -77,9 +92,21 @@ public class TrustStoreController extends AbstractTomcatContainerController {
     return mv;
   }
 
+  private void loadKeyStore(InputStream fis, char[] chars) throws KeyStoreLoadException {
+    try {
+      KeyStore ks = KeyStore.getInstance("JKS");
+      ks.load(fis, chars);
+    } catch (KeyStoreException | NoSuchAlgorithmException | CertificateException e) {
+      mylogger.error("", e);
+    } catch (IOException e) {
+        throw new KeyStoreLoadException(e.getMessage(), e);
+    }
+  }
+
   @Value("truststore")
   @Override
   public void setViewName(String viewName) {
     super.setViewName(viewName);
   }
+
 }
