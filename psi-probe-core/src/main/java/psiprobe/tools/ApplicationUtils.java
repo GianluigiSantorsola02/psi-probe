7d7c8035861c09c435f164cac6205c1dbee89669
GianluigiSantorsola <116060706+GianluigiSantorsola02@users.noreply.github.com>
2024-01-05 18:56:37 +0100

    Update ApplicationUtils.java

--- psi-probe-core/src/main/java/psiprobe/tools/ApplicationUtils.java
+++ psi-probe-core/src/main/java/psiprobe/tools/ApplicationUtils.java
@@ -41,9 +41,6 @@ public final class ApplicationUtils {
   /**
    * Prevent Instantiation.
    */
-  public ApplicationUtils() {
-    // Prevent Instantiation
-  }
 
   /**
    * Gets the application.
@@ -410,10 +407,6 @@ public final class ApplicationUtils {
   public static List<FilterInfo> getApplicationFilters(Context context,
       ContainerWrapperBean containerWrapper) {
     return containerWrapper.getTomcatContainer().getApplicationFilters(context);
-  }
-
-  public static int getCatalinaBase() {
-      return 1;
   }
 
   public static class ApplicationResourcesException extends Exception {
